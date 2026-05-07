a0db867ac92faee73efb3e2411602e4216748e6e
anmawex <corderoan777@gmail.com>
2026-05-07 08:42:19 -0500

    limpiar comentarios

--- src/main/java/com/parameta/empleados/exception/GlobalExceptionHandler.java
+++ src/main/java/com/parameta/empleados/exception/GlobalExceptionHandler.java
@@ -39,36 +39,3 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(cuerpoError, HttpStatus.BAD_REQUEST);
     }
 }
-/*
-public class GlobalExceptionHandler {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Object> manejarValidacionesDeCampos(MethodArgumentNotValidException ex) {
-        Map<String, Object> cuerpoError = new HashMap<>();
-        cuerpoError.put("timestamp", LocalDateTime.now());
-        cuerpoError.put("estado", HttpStatus.BAD_REQUEST.value());
-
-        // Aquí extraemos solo los mensajes que tú escribiste en las anotaciones @NotBlank
-        String mensajes = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(FieldError::getDefaultMessage)
-                .collect(Collectors.joining(", "));
-        cuerpoError.put("mensaje", "Errores de validación: " + mensajes);
-        return new ResponseEntity<>(cuerpoError, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class) // Atrapa específicamente esta excepción
-    public ResponseEntity<Object> manejarValidacion(IllegalArgumentException ex) {
-
-        // Creamos un mapa para que la respuesta sea un JSON ordenado
-        Map<String, Object> cuerpoError = new HashMap<>();
-        cuerpoError.put("timestamp", LocalDateTime.now());
-        cuerpoError.put("mensaje", ex.getMessage()); // Aquí va tu "El empleado debe ser mayor de edad"
-        cuerpoError.put("estado", HttpStatus.BAD_REQUEST.value());
-
-        // Devolvemos un código 400 (Bad Request) de un 500
-        return new ResponseEntity<>(cuerpoError, HttpStatus.BAD_REQUEST);
-    }
-}
-*/
